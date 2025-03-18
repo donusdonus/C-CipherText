4aa04c4c63f5dc7b17817b3b4757821149d81465
Toon.automation@gmail.com <Toon.automation@gmail.com>
2025-03-18 16:30:46 +0700

    test

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main() {
     char data[1023];
     size_t len=0;
     
-    len = sprintf(&data[0],"Edith Rehnborg, wife of Nutrilite founder Carl Rehnborg, founded Edith Rehnborg Cosmetics in 1958, which later became Artistry.[1] In 1972, Nutrilite merged with Amway thereby giving Amway the controlling interest of the Artistry brand.[2] The brand expanded internationally to Australia, Hong Kong, Malaysia, France, the Netherlands, United Kingdom and West Germany.\n");
+    len = sprintf(&data[0],"Little John\n");
 
 
     cipher.encrypt((uint8_t*)&data[0],len,234);
